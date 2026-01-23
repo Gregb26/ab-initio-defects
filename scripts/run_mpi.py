14425ac8c28835fb995d0342c996c337a974f323
gregb26 <gregoire.barrette@umontreal.ca>
2026-01-23 16:41:33 -0500

    added timing

--- scripts/run_mpi.py
+++ scripts/run_mpi.py
@@ -6,6 +6,8 @@ os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
 os.environ.setdefault("NUMEXPR_NUM_THREADS", "1")
 
 import numpy as np
+import time
+
 from mpi4py import MPI
 
 from electron_defect_interaction.defects.local_G import (
@@ -19,16 +21,17 @@ def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
-    wfk_uc = "wfk_uc.nc"
-    wfk_p  = "wfk_p.nc"   
-    wfk_d  = "wfk_d.nc"   
-    pot_p  = "pot_p.nc"
-    pot_d  = "pot_d.nc"
-    psp8   = "C.psp8"
-
     # Local part: all ranks participate, only rank 0 gets the full ML_G
     prep = None
     if rank == 0:
+        t0 = time.time()
+        print("before local", flush=True)
+        wfk_uc = "wfk_uc.nc"
+        wfk_p  = "wfk_p.nc"   
+        wfk_d  = "wfk_d.nc"   
+        pot_p  = "pot_p.nc"
+        pot_d  = "pot_d.nc"
+        psp8   = "C.psp8"
         prep = prep_reciprocal_inputs(
         wfk_uc, wfk_d, pot_p, pot_d,
         subtract_mean=False, pristine=False
@@ -36,14 +39,18 @@ def main():
     prep = comm.bcast(prep, root=0)
 
     ML_G = compute_ML_G_mpi(prep, block_size=128, show_tqdm=(rank == 0))
+    if rank == 0:
+        print('after local: ', time.time()-t0, flush=True)
 
     # Non-local part: compute only on rank 0
     if rank == 0:
+        print('before non-local:', time.time()-t0, flush=True)
         M_NL = compute_M_NL(wfk_uc, wfk_d, psp8) - compute_M_NL(wfk_uc, wfk_p, psp8)
-
+        print('after non-local: ', time.time()-t0, flush=True)
         M = ML_G + M_NL
+        print('after adding: ', time.time()-t0, flush=True)
         np.save("M_test.npy", M)
-
+        print('after saving: ', time.time()-t0, flush=True)
     comm.Barrier()
 
 

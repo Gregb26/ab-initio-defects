69d2773e50f876419ff17762e921a966999f6c84
gregb26 <gregoire.barrette@umontreal.ca>
2026-02-04 10:35:46 -0500

    fixed a bug

--- src/electron_defect_interaction/defects/local_G.py
+++ src/electron_defect_interaction/defects/local_G.py
@@ -344,9 +344,9 @@ def compute_block_M_interp(Ved_G, ngfft, delta_k_sc, Gp, G, Ckp, Ck0, block_size
         Ckp_blk = Ckp[:, start:stop]          # (nband,B)
 
         # compute q and convert to grid index
-        qx = (delta_k_sc[0] + Gp_blk[:, None, 0] - G[None, :, 0]) * Nx
-        qy = (delta_k_sc[1] + Gp_blk[:, None, 1] - G[None, :, 1]) * Ny
-        qz = (delta_k_sc[2] + Gp_blk[:, None, 2] - G[None, :, 2]) * Nz
+        qx = np.mod(delta_k_sc[0] * Nx + (Gp_blk[:, None, 0] - G[None, :, 0]), Nx)
+        qy = np.mod(delta_k_sc[1] * Ny + (Gp_blk[:, None, 1] - G[None, :, 1]), Ny)
+        qz = np.mod(delta_k_sc[2] * Nz + (Gp_blk[:, None, 2] - G[None, :, 2]), Nz)
 
         Vblk = cubic_spline_periodic(Ved_G, qx, qy, qz)            # (B, nG_k)
         tmp  = Vblk @ Ck0.T                   # (B, nband)
